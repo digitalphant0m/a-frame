a28042a53344ce975845d2d7c8032d09f547e454
digitalphant0m <digitalphant0m@users.noreply.github.com>
2018-06-12 15:09:19 -0400

    Update setup.sh
    updated scripts

--- setup.sh
+++ setup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 echo '......................'
-echo '<=:Welcome to A-Framework:=>'
+echo '<=:WELCOME TO POSTHASTE:=>'
 echo '......................'
 
 printf '\n'
-echo 'Copying Files to:=>' $1
+echo 'COPYING FILES TO:=>' $1
 printf '\n'
 
 cd ../
 
 mkdir $1
 
-cd af/origin
+cd posthaste/origin
 
 cp {package.json,server.js,webpack.config.js} ../../$1
 
@@ -20,9 +20,9 @@ cp -r {dist,src} ../../$1
 
 cd ../../$1
 
-echo '<=:Installing necessary dependencies:=>'
+echo '<=:INSTALLING NECESSARY DEPENDENCIES:=>'
 npm i
 
 
-echo 'Files copied to:=> '  
+echo 'FILES ARE COPIED TO:=> '  
 pwd
